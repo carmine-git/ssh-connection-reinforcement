1a7abd1088e8c9c443c99a896eb10c63b97ff2da
carmine-git <amaryllismain09@gmail.com>
2023-10-03 17:22:18 +0200

    Feat(install): Writing to sshd file

--- install.py
+++ install.py
@@ -12,3 +12,8 @@ package_name = "libpam-google-authenticator"
 print(f"Installing {package_name}...")
 os.system(f"sudo apt update && sudo apt install {package_name}")
 
+base_path = "etc/"
+print(f"Writing to {base_path}pam.d/sshd")
+with os.popen(f"{base_path}pam.d/sshd", "a") as ssh_file:
+    ssh_file.write("auth required pam_google_authenticator.so")
+
